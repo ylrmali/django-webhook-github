a8771d3327792d70cf248039f12853abba64dd08
Ali <ylrmali1289@gmail.com>
2023-09-12 22:08:07 +0300

    git pull command added

--- auto/main/views.py
+++ auto/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import os
 # Create your views here.
 
 
@@ -13,8 +13,7 @@ from django.views.decorators.csrf import csrf_exempt
 def git(request):
     if request.method == 'POST':
         payload = request.body
-        print('a')
-        print(payload)
+        os.system('cd /home/ubuntu/test/ && git pull origin main')
         return HttpResponse(payload)
 
     return HttpResponse('Hello, world. You\'re at the git index.')
