146c752c5fa8a3966024369c4d3856331b1f3968
Ali <ylrmali1289@gmail.com>
2023-09-12 17:14:46 +0300

    webhook config

--- auto/main/views.py
+++ auto/main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def git(request):
+    if request.method == 'POST':
+        payload = request.json
+        return HttpResponse(payload)
+
+    return HttpResponse('Hello, world. You\'re at the git index.')
 
