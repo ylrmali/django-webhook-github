374f25b2e1b265c4cdd068edb1b7faabd1ac2077
Ali <ylrmali1289@gmail.com>
2023-09-12 17:36:34 +0300

    s

--- auto/main/views.py
+++ auto/main/views.py
@@ -12,7 +12,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def git(request):
     if request.method == 'POST':
-        payload = request.json
+        payload = request
         print('anan')
         print(payload)
         return HttpResponse(payload)
